23160349b2b06fe29538ed9774afe06072167f51
jcvhrocker-glitch <jcvhrocker@gmail.com>
2025-10-19 13:53:17 -0600

    update

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
 import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
+car_data = pd.read_csv('vehicles_us.csv')
+
+# Crear un histograma utilizando plotly.graph_objects
+# Se crea una figura vacía y luego se añade un rastro de histograma
+fig = go.Figure(data=[go.Histogram(x=car_data['odometer'])])
+
+# Opcional: Puedes añadir un título al gráfico si lo deseas
+fig.update_layout(title_text='Distribución del Odómetro')
+
+# Mostrar el gráfico Plotly
+fig.show()
